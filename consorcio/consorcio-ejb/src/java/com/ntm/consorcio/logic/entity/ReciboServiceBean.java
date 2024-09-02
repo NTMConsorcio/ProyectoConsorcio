cbc29b55d80175b80c0e0e881cf014a5619e2ccc
unknown <sorbello.mauro.em.cn@gmail.com>
2024-09-02 20:09:22 -0300

    Paquetes inicializados

---
+++ consorcio/consorcio-ejb/src/java/com/ntm/consorcio/logic/entity/ReciboServiceBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ntm.consorcio.logic.entity;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Mauro Sorbello
+ */
+@Stateless
+@LocalBean
+public class ReciboServiceBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
